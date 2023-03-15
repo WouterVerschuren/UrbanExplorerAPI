2c9b138b6219a80386126077be09ebf475fb0400
WouterVerschuren <finwou@gmail.com>
2023-03-15 22:46:11 +0100

    Add project files.

---
+++ UrbanExplorerDAL/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanExplorerDAL
+{
+    internal static class DatabaseConnection
+    {
+        public static MySqlConnection CreateConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(GetConnectionString());
+
+            return connection;
+        }
+
+        public static string GetConnectionString()
+        {
+            return "";
+        }
+    }
+}
